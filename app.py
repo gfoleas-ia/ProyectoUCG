fd9bc3694bd1d97834b6fd67263cb937d310e49b
gfoleas-ia <gfoleas@gmail.com>
2026-05-16 10:03:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import streamlit as st
 st.title("proyecto final Casa Grande")
 st.sidebar.title("Parametros")
 st.image("Python_logo")
+import libreria_funciones as lf
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+  
